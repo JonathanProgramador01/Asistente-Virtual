a1b5820640f5b9c276f973dc3b2a15a5edd4279a
Jonathan Rosas Cedillo <157512868+JonathanProgramador01@users.noreply.github.com>
2024-09-21 12:51:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,90 @@
 from tkinter import *
 import pyttsx3
-import difflib
+from difflib import get_close_matches
+
+
+
 
 
 class AsistenteVirtual:
+
     def __init__(self):
+        self.engine = pyttsx3.init()
+        rate = self.engine.getProperty("rate")
+        self.engine.setProperty("rate",rate-68)
         self.responses = {
-           
-    "que es el deporte": "El deporte es una actividad física que se realiza de manera competitiva o recreativa, siguiendo un conjunto de reglas.",
-    "cual es el deporte mas famoso": "El deporte más famoso del mundo es el fútbol.",
-    "que es el futbol": "El fútbol es un deporte en el que dos equipos intentan marcar goles al introducir una pelota en la portería del equipo contrario.",
-    "que es basketball": "El baloncesto es un deporte en el que dos equipos intentan anotar puntos lanzando una pelota a través de un aro elevado.",
-    "porque es bueno hacer deporte": "Hacer deporte es bueno porque mejora la salud física, aumenta la energía, reduce el estrés y promueve la socialización.",
-    "que es el futbol americano": "El fútbol americano es un deporte de equipo en el que dos equipos intentan avanzar con un balón ovalado hacia la zona de anotación del oponente.",
-    "es malo si no hago deporte": "No hacer deporte puede tener efectos negativos en la salud física y mental, como aumento de peso y estrés.",
-    "como podria empezar": "Puedes empezar haciendo caminatas, uniéndote a una clase de ejercicio o practicando un deporte que te guste.",
-    "cuales son los beneficios del deporte": "Los beneficios del deporte incluyen una mejor salud cardiovascular, aumento de la fuerza muscular y mejor salud mental.",
-    "que son los juegos olimpicos": "Los Juegos Olímpicos son un evento deportivo internacional que reúne a atletas de todo el mundo en diversas disciplinas.",
-    "que es el entrenamiento de resistencia": "El entrenamiento de resistencia es una forma de ejercicio que utiliza la resistencia para mejorar la fuerza y la resistencia muscular.",
-    "cual es la importancia del trabajo en equipo en el deporte": "El trabajo en equipo es crucial en muchos deportes, ya que fomenta la cooperación, la comunicación y la estrategia entre los jugadores."
-
-
+            "saludame mi nombre es":"Holaa, ",
+            "que es el deporte": "El deporte es una actividad física que se realiza de manera competitiva o recreativa, siguiendo un conjunto de reglas.",
+            "cual es el deporte mas famoso": "El deporte más famoso del mundo es el fútbol.",
+            "que es el futbol": "El fútbol es un deporte en el que dos equipos intentan marcar goles al introducir una pelota en la portería del equipo contrario.",
+            "que es basketball": "El baloncesto es un deporte en el que dos equipos intentan anotar puntos lanzando una pelota a través de un aro elevado.",
+            "porque es bueno hacer deporte": "Hacer deporte es bueno porque mejora la salud física, aumenta la energía, reduce el estrés y promueve la socialización.",
+            "que es el futbol americano": "El fútbol americano es un deporte de equipo en el que dos equipos intentan avanzar con un balón ovalado hacia la zona de anotación del oponente.",
+            "es malo si no hago deporte": "No hacer deporte puede tener efectos negativos en la salud física y mental, como aumento de peso y estrés.",
+            "como podria empezar": "Puedes empezar haciendo caminatas, uniéndote a una clase de ejercicio o practicando un deporte que te guste.",
+            "cuales son los beneficios del deporte": "Los beneficios del deporte incluyen una mejor salud cardiovascular, aumento de la fuerza muscular y mejor salud mental.",
+            "que son los juegos olimpicos": "Los Juegos Olímpicos son un evento deportivo internacional que reúne a atletas de todo el mundo en diversas disciplinas.",
+            "que es el entrenamiento de resistencia": "El entrenamiento de resistencia es una forma de ejercicio que utiliza la resistencia para mejorar la fuerza y la resistencia muscular.",
+            "cual es la importancia del trabajo en equipo en el deporte": "El trabajo en equipo es crucial en muchos deportes, ya que fomenta la cooperación, la comunicación y la estrategia entre los jugadores."
 
         }
-        self.engine = pyttsx3.init()
-        self.set_voice()
 
-    def set_voice(self, voice_name="spanish-latin-am"):
-        voices = self.engine.getProperty('voices')
-        for voice in voices:
-            if voice_name in voice.id:
-                self.engine.setProperty('voice', voice.id)
-                break
-    def get_response(self, user_input):
-        user_input = user_input.lower()  # Convertir a minúsculas
-        user_input = user_input.replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú",
-                                                                                                                "u").replace(
-            "ñ", "n")
+    def talk(self,text):
 
-        if user_input in self.responses:
-            return self.responses[user_input]
-
-        closest_matches = difflib.get_close_matches(user_input, self.responses.keys(), n=1, cutoff=0.5)
-        if closest_matches:
-            return self.responses[closest_matches[0]]
-        else:
-            return "No tengo una respuesta exacta para eso, pero estoy aquí para ayudar en lo que pueda."
-
-    def speak_response(self, response):
-        self.engine.say(response)
+        self.engine.say(text)
         self.engine.runAndWait()
 
+    def matches(self,text):
+        text = text.lower()
+        text = (text.replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").
+        replace("ú","u").replace("ñ", "n"))
+
+        if text in self.responses:
+            return self.responses[text]
+
+
+        best_match = get_close_matches(text,self.responses.keys())
+        if best_match:
+            return  self.responses[best_match[0]]
+        else:
+            return "Lo siento pero no cuento con esa informacion"
+class Gui:
+    def __init__(self, pantalla):
+
+        self.asistentevirtual = AsistenteVirtual()
+        self.window = pantalla
+        self.window.title("Asistente Virtual De Deportes")
+        self.window.geometry("720x650")
+        self.canva = Canvas(self.window,highlightthickness=0, width=720, height=650)
+        self.textoid = None
+        self.entrada()
+        self.crear_botones()
+
+
+    def crear_fondo(self):
+        ##Despues pongo esta imagen de fondo
+        self.canva.create_text(360, 50, text="Asistente Virtual", font=("Elephant", 30, "bold"),
+                          fill="#C7C3B4")  # Esto es para poner mi texto
+        self.canva.pack()
+        self.response = Text(fg="#C7C3B4", font=("Arial", 15, "bold"), width=58, height=17, bg="#0F0F0C")
+        self.response.place(x=40, y=100)
+
+    def procesador(self):
+        text = self.data.get("1.0", END).strip()
+
+        if text== "Escribe aquí..." or text == "":
+            return
+        response = self.asistentevirtual.matches(text)
+
+        if response == "Holaa, ":
+            temp =" ".join(text.split()[4:])
+            response += temp
+
+        self.window.update_idletasks()
+        self.asistentevirtual.talk(response)
+        self.put_text(response, text)
+
+        self.data.delete("1.0", END)
 
 
 
@@ -58,54 +92,48 @@ class AsistenteVirtual:
 
 
 
+    def put_text(self,asistente,ususario):
+
+        self.response.tag_configure("padding", lmargin1=30, lmargin2=30, rmargin=30)
+        text = f"Usuario: {ususario}\n"
+        text2 = f"Asistente: {asistente}\n\n\n"
+        self.response.insert(END, text)
+        self.response.insert(END, text2)
+        self.response.tag_add("padding", "1.0", "end")
 
 
-def on_focus_in(event):
-    if data.get("1.0", "end-1c") == "Escribe aquí...":
-        data.delete("1.0", END)
-        data.config(fg="#6D1A12")
-def on_focus_out(event):
-    if data.get("1.0", "end-1c") == "":
-        data.insert("1.0", "Escribe aquí...")
-        data.config(fg="grey")
-def get_data(data):
-    print(data.get("1.0", END))
+        # Boton para saludar mi imagen
+    def crear_botones(self):
+
+        self.button_enviar = Button(text="Enviar", fg="#C7C3B4", font=("Arial", 15, "bold"), highlightthickness=0,
+                               bg="#0F0F0C", width=9,command=self.procesador)
+        self.button_enviar.place(y=605, x=600)
+    def on_focus_in(self,event):
+        if self.data.get("1.0", "end-1c") == "Escribe aquí...":
+            self.data.delete("1.0", END)
+            self.data.config(fg="#C7C3B4")
+    def on_focus_out(self,event):
+        if self.data.get("1.0", "end-1c") == "":
+            self.data.insert("1.0", "Escribe aquí...")
+            self.data.config(fg="#C7C3B4")
+    def entrada(self):
+        self.data = Text(fg="#C7C3B4", font=("Arial", 15, "bold"), width=66, height=5,bg="#0F0F0C")
+        self.data.insert("1.0", "Escribe aquí...")
+        self.data.bind("<FocusIn>", self.on_focus_in)
+        self.data.bind("<FocusOut>", self.on_focus_out)
+        self.data.place(x=0, y=550)
+
+
+
+
+
+
+
 
 window = Tk()
-engine = pyttsx3.init()
-rate = engine.getProperty("rate")
-engine.setProperty("rate",rate-95)
-
-
-
-window.geometry("500x650")
-
-canva = Canvas(window, width=500, height=700)
-canva.create_text(90, 30, text="Asistente Virtual", font=("Arial", 20, "bold"), anchor="w")
-canva.pack()
-
-chat_image = PhotoImage(file="chatbot_image.png")
-canva.create_image(50, 30, image=chat_image)
-
-data = Text(fg="grey", font=("Arial", 20, "bold"), width=33, height=3)
-data.insert("1.0", "Escribe aquí...")
-data.bind("<FocusIn>", on_focus_in)
-data.bind("<FocusOut>", on_focus_out)
-data.place(x=0, y=550)
-
-
-button = Button(text="Enviar", font=("Arial", 15, "bold"), highlightthickness=0,command=lambda: get_data(data))
-button.place(x=422, y=610)
-
-
-
-
-
-
-
-
-
-
-
+gui = Gui(window)
+background_image = PhotoImage(file="./image.png")
+gui.canva.create_image(360, 325, image=background_image)
+gui.crear_fondo()
 
 window.mainloop()
